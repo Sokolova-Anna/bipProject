dbf3ded031fdfd038ce1f4e25d592ce3cd24b886
Milana Khairullina <milanakhairullina@gmail.com>
2024-08-16 14:18:51 +0300

    add: add yandex api map

--- js/script.js
+++ js/script.js
@@ -1,46 +1,11 @@
-
-
-ymaps.ready(function () {
-    let myMap = new ymaps.Map("YMapsID", {
-      center: [59.93, 30.31], //saint-p
-      zoom: 10
-    });
-
-    let myPlacemark1 = new ymaps.Placemark([59.963966, 30.302487], {
-        balloonContent: 'Color Cafe'
-    }, {
-        iconLayout: 'default#image', // Указываем, что используем свое изображение
-        iconImageHref: './images/dog-friendly.png', // Ссылка на ваше изображение
-        iconImageSize: [26, 26], // Размеры изображения (ширина, высота)
-        iconImageOffset: [-15, -42] // Смещение изображения (по оси X и Y), чтобы центрировать его относительно точки
-    });
-    
-
-    let myPlacemark2 = new ymaps.Placemark([59.946200, 30.373191], {
-        balloonContent: 'Таврический сад'
-    }, {
-        iconLayout: 'default#image', // Указываем, что используем свое изображение
-        iconImageHref: './images/park_mark.png', // Ссылка на ваше изображение
-        iconImageSize: [26, 26], // Размеры изображения (ширина, высота)
-        iconImageOffset: [-15, -42] // Смещение изображения (по оси X и Y), чтобы центрировать его относительно точки
-    });
-
-
-    myMap.geoObjects.add(myPlacemark1);
-    myMap.geoObjects.add(myPlacemark2);
-  });
-
-
-  //59.946200, 30.373191
-
-
-//--кнопка регистрации на главном экране
-const ButtonAuth = document.querySelectorAll('.description__auth');
+const buttonAuthElement  = document.querySelector('.description__auth');
 const authForm = document.querySelector('.alert') 
-ButtonAuth.forEach(item => { item.addEventListener("click", (e) => {
+if (buttonAuthElement ) {
+    buttonAuthElement .addEventListener("click", (e) => {
     e.preventDefault(); 
     authForm.classList.remove("hide");
-}); });
+}); 
+}
 
 
 const ButtonCloseAuth = document.querySelector('.alert__close');
@@ -52,11 +17,15 @@ if (ButtonCloseAuth) {
 }
 
 const ButtonHeaderMap = document.querySelector('.header__map');
-ButtonHeaderMap.addEventListener("click", () => {
-    location='map.html'
-})
+if (ButtonHeaderMap) {
+    ButtonHeaderMap.addEventListener("click", () => {
+        location='map.html'
+    })
+}
 
 const ButtonHeaderMain = document.querySelector('.header__main');
-ButtonHeaderMain.addEventListener("click", () => {
-    location='index.html'
-})
+if (ButtonHeaderMain) {
+    ButtonHeaderMain.addEventListener("click", () => {
+        location='index.html'
+    })
+}
--- js/script_map.js
+++ js/script_map.js
@@ -1,28 +1,45 @@
 
 //--кнопка регистрации на главном экране
-const ButtonAuth = document.querySelectorAll('.description__auth');
-const authForm = document.querySelector('.alert') 
-ButtonAuth.forEach(item => { item.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    authForm.classList.remove("hide");
-}); });
+
+ymaps.ready(function () {
+    let myMap = new ymaps.Map("YMapsID", {
+      center: [59.93, 30.31], //saint-p
+      zoom: 10
+    });
+
+    let myPlacemark1 = new ymaps.Placemark([59.963966, 30.302487], {
+        balloonContent: 'Color Cafe'
+    }, {
+        iconLayout: 'default#image', // Указываем, что используем свое изображение
+        iconImageHref: './images/dog-friendly.png', // Ссылка на ваше изображение
+        iconImageSize: [26, 26], // Размеры изображения (ширина, высота)
+        iconImageOffset: [-15, -42] // Смещение изображения (по оси X и Y), чтобы центрировать его относительно точки
+    });
+    
+
+    let myPlacemark2 = new ymaps.Placemark([59.946200, 30.373191], {
+        balloonContent: 'Таврический сад'
+    }, {
+        iconLayout: 'default#image', // Указываем, что используем свое изображение
+        iconImageHref: './images/park_mark.png', // Ссылка на ваше изображение
+        iconImageSize: [26, 26], // Размеры изображения (ширина, высота)
+        iconImageOffset: [-15, -42] // Смещение изображения (по оси X и Y), чтобы центрировать его относительно точки
+    });
 
 
-const ButtonCloseAuth = document.querySelector('.alert__close');
-ButtonCloseAuth.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    authForm.classList.add("hide");   
-})
+    myMap.geoObjects.add(myPlacemark1);
+    myMap.geoObjects.add(myPlacemark2);
+  });
 
-const ButtonHeaderMap = document.querySelector('.header__map');
-ButtonHeaderMap.addEventListener("click", () => {
-    location='map.html'
-})
-
-const ButtonHeaderMain = document.querySelector('.header__main');
-ButtonHeaderMain.addEventListener("click", () => {
-    location='index.html'
-})
-
-
+//const ButtonHeaderMap = document.querySelector('.header__map');
+//ButtonHeaderMap.addEventListener("click", () => {
+//    location='map.html'
+//})
+//
+//const ButtonHeaderMain = document.querySelector('.header__main');
+//ButtonHeaderMain.addEventListener("click", () => {
+//    location='index.html'
+//})
+//
+//
 
