8124b940a432e849ad8cfc5352d7fd1b6c96a2d0
Milana Khairullina <milanakhairullina@gmail.com>
2024-08-17 12:44:06 +0300

    add: add event for click to mark at map

--- js/script_map.js
+++ js/script_map.js
@@ -1,3 +1,5 @@
+let listMarks = []
+
 ymaps.ready(function () {
     let myMap = new ymaps.Map("YMapsID", {
       center: [59.93, 30.31], //saint-p
@@ -23,7 +25,31 @@ ymaps.ready(function () {
         iconImageOffset: [-15, -42] // Смещение изображения (по оси X и Y), чтобы центрировать его относительно точки
     });
 
+    let myPlacemark3 = new ymaps.Placemark([59.991900, 30.216425], {
+        balloonContent: 'Яхтенный сквер'
+    }, {
+        iconLayout: 'default#image', // Указываем, что используем свое изображение
+        iconImageHref: './images/mark_commands.png', // Ссылка на ваше изображение
+        iconImageSize: [26, 26], // Размеры изображения (ширина, высота)
+        iconImageOffset: [-15, -42] // Смещение изображения (по оси X и Y), чтобы центрировать его относительно точки
+    });
+
 
     myMap.geoObjects.add(myPlacemark1);
     myMap.geoObjects.add(myPlacemark2);
+    myMap.geoObjects.add(myPlacemark3);
+
+    listMarks.push(myPlacemark1);
+    listMarks.push(myPlacemark2);
+    listMarks.push(myPlacemark3);
+
+
+    listMarks.forEach(item => { item.events.add("click", (e) => {
+        e.preventDefault();
+        let coords = e.get('coords');   
+        console.log(coords) 
+
+    }); });
+
   });
+
